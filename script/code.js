bd0525853071334153cfdab7c960e51e83ab4af1
LyleBrown010 <lylebrown010@gmail.com>
2023-05-16 16:04:35 +0200

    Exercise done

---
+++ script/code.js
@@ -0,0 +1,9 @@
+const subjects = ['Python', 'C++', 'VB.Net', 'Javascript', 'Assembly']
+
+// #1 last element 
+
+let lastElement = subjects.pop(); 
+console.log(lastElement);
+
+// #2 middle element
+console.log('Middle Element:', subjects[2])
